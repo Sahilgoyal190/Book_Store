8f4a8114f29cd6456e817be1dec78644113229b8
amangal <Password@123>
2018-01-24 20:09:57 +0530

    removing Readme.md
    removing Readme.md

--- src/component/book.js
+++ src/component/book.js
@@ -1,7 +1,4 @@
 import React from 'react';
-
-
-
 export const Book = ({ book, index, addToCart }) => {
     return (
         <div className="col-md-3 book">
@@ -13,7 +10,7 @@ export const Book = ({ book, index, addToCart }) => {
             <div>Author: {book.author}</div>
             <div>Inventory: {book.count}</div>
 
-            <input className="btn btn-primary" type="button" value="Add To Cart" onClick={()=>addToCart(book,index)}/>
+            <input className="btn btn-primar" type="button" value="Add To Cart" onClick={()=>addToCart(book,index)}/>
         </div>
     )
 }   
